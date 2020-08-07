01ef6fbc5fc7139fbf64106748cc29f26587bad4
ldemaz <3401881+ldemaz@users.noreply.github.com>
2020-08-07 11:55:10 -0400

    Updated analyses and SI, manuscript text to include results from active versus random training

--- data-raw/data_from_instances.R
+++ data-raw/data_from_instances.R
@@ -430,11 +430,12 @@ delta_func <- function(x, rnd = 4) round((x - lag(x)) / lag(x), rnd)
 # process individual AOIs
 metrics <- c("Accuracy", "AUC", "F1", "TSS", "Precision", "Recall", "TPR",
              "FPR")
-aoinms <- paste0("labeller", 1:16)
+aoinms <- paste0("labeller", c(1:16, "1r", "8r", "15r", "8v2", "82019"))
 iteration_metrics <- instance_tbls$iteration_metrics %>%
   filter(aoi %in% aoinms) %>%
   filter(iteration > 0) %>%
-  filter(!(aoi == "labeller3" & run == 0))  %>% # remove run 0 labeller3
+  filter(!(aoi == "labeller3" & run == 0)) %>% # remove run 0 labeller3
+  filter(!(aoi %in% paste0("labeller", c("1r", "8r")) & run == 1)) %>% # no run1
   mutate(iteration = iteration - 1) %>% # rebase iteration counter on 0
   mutate(F1 = 2 * (precision * recall) / (precision + recall)) %>%
   rename(AOI = aoi, Iteration = iteration, Accuracy = accuracy, AUC = auc,
@@ -442,21 +443,24 @@ iteration_metrics <- instance_tbls$iteration_metrics %>%
          FPR = fpr) %>%
   select(AOI, Iteration, !!metrics) %>%
   group_by(AOI) %>%  # calculate deltas
-  mutate_at(metrics, funs("change" = delta_func)) %>%
+  mutate_at(metrics, list("change" = delta_func)) %>%
   ungroup() %>%
   tidyr::pivot_longer(cols = Accuracy:FPR_change, names_to = "Metric",
                       values_to = "Score")
 
 # process across AOI means and combine with AOIs
+aois <- paste0("labeller", 1:16)  # calculate summary across just AOIs 1-16
 iteration_metrics %>%
+  filter(AOI %in% aois) %>%
   filter(Iteration < 4) %>%
-  group_by(Iteration, Metric) %>% summarize(Score = mean(Score)) %>%
+  group_by(Iteration, Metric) %>%
+  summarize(Score = mean(Score)) %>%
   mutate(AOI = "All") %>%
   ungroup %>%
   select(AOI, !!names(.)) %>%
   bind_rows(iteration_metrics, .) -> iteration_metrics
 
-usethis::use_data(iteration_metrics)
+usethis::use_data(iteration_metrics, overwrite = TRUE)
 
 #------------------------------------------------------------------------------#
 # Extract maps for validation sites of most accurate labeller for each AOI
