d64183e70515088e6fb18ab2dd58bfdd6b73c046
ldemaz <3401881+ldemaz@users.noreply.github.com>
2020-08-14 15:44:27 -0400

    Label risk assessment completed and integrated into SI and main text.

--- data-raw/data_from_instances.R
+++ data-raw/data_from_instances.R
@@ -392,12 +392,14 @@ label_summary <- list("scores" = quality_scores, "score_stats" = quality_sum,
 usethis::use_data(label_summary, overwrite = TRUE)
 
 #------------------------------------------------------------------------------#
-# Consensus labelling conflicts (needs updating when consensus conflicts are
-# fixed)
-
-# reading in from separate script
-
+# Consensus labelling conflicts.
+# Processed for all AOIs in consensus_risk.R
+load(system.file("extdata/", "consensus_risk.rda", package = "activemapper"))
 load(system.file("extdata/", "instance_tbls.rda", package = "activemapper"))
+
+mgrid <- data.table::fread(
+  system.file("extdata/ghana_grid.csv", package = "activemapper")
+)
 
 # select AOIs and unique names:
 aoinms <- c(
@@ -405,21 +407,68 @@ aoinms <- c(
   paste0("labeller", c("trainsouth", "trainsouthnl", "trainsw", 1:16))
 )
 
-nms <- unique(c(train_val_sites$initial$name, train_val_sites$active$name))
-# any(!nms %in% unique(instance_tbls$kml_data$name))
+# get usages
+usages <- instance_tbls$incoming_names %>%
+  filter(!(aoi == "labeller3" & run == 0)) %>%
+  filter(label == TRUE) %>%
+  filter(aoi %in% aoinms) %>%
+  dplyr::select(aoi, name, usage) %>%
+  left_join(., mgrid %>% dplyr::select(name, x, y), by = "name")
 
-# This selects out F sites from the mapped list
-label_consensus <- instance_tbls$kml_data %>% filter(aoi %in% aoinms) %>%
-  filter(name %in% nms) %>%
-  select(-one_of("kml_type", "mapped_count", "mappers_needed")) %>%
-  rename(Risk = consensus_conflict)
+# checks on duplicates
+# dupnms <- usages %>% group_by(name) %>% count() %>% filter(n > 1) %>% pull(name)
+# length(dupnms)  # 503 duplicates
+# dupnms2 <- usages %>% filter(name %in% dupnms) %>% arrange(name) %>%
+#   filter(!aoi %in% c("labellertrainsouth", "labellertrainsouthnl"))
+# nrow(dupnms2) # 7 of those in other AOIs besides 2 runs of labellertrainsouth
+# usages %>% filter(name == "GH0328463")
+# this site was validate in labeller7 and in both labellertrainsouth versions
 
-# label_consensus %>% filter(aoi == "labeller8") %>% filter(Risk > 0)
-# label_consensus %>% filter(aoi == "labeller8") %>% filter(Risk > 0)
-# label_consensus %>% group_by(aoi) %>%
-#   summarize(zeros = sum(ifelse(Risk == 0, 1, 0)),
-#             nonzeros = sum(ifelse(Risk > 0, 1, 0)))
-usethis::use_data(label_consensus, overwrite = TRUE)
+label_risk_pts <- left_join(consensus_risk, usages, by = c("aoi", "name")) %>%
+  dplyr::select(aoi, name, usage, !!names(.))
+# check on duplicates
+# nm <- label_risk %>% group_by(name) %>% count() %>% filter(n > 1) %>% pull(name)
+# label_risk %>% filter(name %in% nm) %>% arrange(name) %>% View()
+# label_risk %>%
+#   filter(name %in% nm) %>%
+#   filter(!aoi %in% c("labellertrainsouth", "labellertrainsouthnl")) %>%
+#   dplyr::select(aoi, name, usage) #%>% distinct(name)
+# label_risk %>% filter(name == "GH0328463")
+# same as from usage
+
+# Rename some columns and values
+clusters <- paste0("labeller", c("", "trainsouth", "trainsouthnl", "trainsw"))
+new_cluster_nms <- paste0("Cl", c("1", "2a", "2b", "3"))
+
+label_risk_pts <- label_risk_pts %>%
+  rename(Risk = meanrisk, Riskprop = prop_risky) %>%
+  mutate(usage = stringr::str_to_title(usage)) %>%
+  mutate(aoi = case_when(
+    aoi == clusters[1] ~ "-3",
+    aoi == clusters[2] ~ "-2",
+    aoi == clusters[3] ~ "-1",
+    aoi == clusters[4] ~ "0",
+    TRUE ~ gsub("labeller", "", aoi)
+  )) %>%
+  mutate(ord = as.numeric(aoi)) %>% arrange(ord) %>%
+  mutate(aoi = case_when(
+    aoi == "-3" ~ new_cluster_nms[1],
+    aoi == "-2" ~ new_cluster_nms[2],
+    aoi == "-1" ~ new_cluster_nms[3],
+    aoi %in% "0" ~ new_cluster_nms[4],
+    TRUE ~ aoi
+  )) %>%
+  mutate(aoi = forcats::fct_reorder(aoi, ord)) %>%
+  select(-ord)
+
+# summarize
+label_risk_stats <- label_risk_pts %>% group_by(aoi, usage) %>%
+  summarize(Risk = mean(Risk, na.rm = TRUE)) %>%
+  ungroup()
+
+label_risk <- list(pts = label_risk_pts, stats = label_risk_stats)
+
+usethis::use_data(label_risk, overwrite = TRUE)
 
 #------------------------------------------------------------------------------#
 # Iteration metrics
