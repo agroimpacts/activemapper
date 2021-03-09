059410d325b794aa6f693254519d5a5bb90b4c80
Lyndon Estes <lestes@GEO-007840.local>
2021-03-09 17:29:38 -0500

    Edited ms and si: first upload to EarthArXiv. Edited scripts for data_for_instances.R, added new script to data-raw (aois_tiles.R) to show creation of degree and tiling grids

--- data-raw/data_for_instances.R
+++ data-raw/data_for_instances.R
@@ -1,4 +1,5 @@
 # Data set up
+## Follows on from aoi_tiles.R
 
 #------------------------------------------------------------------------------#
 # Prepare main grid for Ghana
@@ -9,16 +10,19 @@ library(sf)
 library(dplyr)
 
 # get data from labeller (get from S3)
-data_path <- file.path("/Users", unname(Sys.info()["user"]),
-                       "Dropbox/projects/activelearning/mapper/spatial/data")
-master_grid <- brick(file.path(data_path, "processed/master_grid.tif"))
-v <- values(master_grid[[1]])
+mgrid_path <- file.path("/vsis3/activemapper/grid/master_grid.tif")
+mgrid <- raster(mgrid_path)
+# data_path <- file.path("/Users", unname(Sys.info()["user"]),
+#                        "Dropbox/projects/activelearning/mapper/spatial/data")
+# mgrid <- brick(file.path(data_path, "processed/master_grid.tif"))
+# v <- values(mgrid[[1]])
 # length(v[!is.na(v)])
 # length(unique(v[!is.na(v)]))
 
 # crop master_grid to ghana_grid
-tiles <- st_read("inst/extdata/ghana_tiles.geojson")
-ghana_grid <- crop(master_grid[[1]], extent(tiles))
+tiles <- st_read(here::here("external/data/ghana_tiles.geojson"))
+# tiles <- tiles %>% rename(aoi = aoi1)
+ghana_grid <- crop(mgrid[[1]], extent(tiles))
 # length(ghana_grid[(!is.na(ghana_grid))])
 # length(ghana_grid[!is.na(ghana_grid)])
 # length(unique(ghana_grid[!is.na(ghana_grid)]))
@@ -26,16 +30,14 @@ ghana_grid <- crop(master_grid[[1]], extent(tiles))
 
 writeRaster(ghana_grid, file = "inst/extdata/ghana_grid.tif",
             overwrite = TRUE, datatype = "INT4U")  # careful for datatype
-r <- raster("inst/extdata/ghana_grid.tif")
+# r <- raster("inst/extdata/ghana_grid.tif")
 # length(r[!is.na(r)])
 # length(unique(r[!is.na(r)]))
 
 #------------------------------------------------------------------------------#
 # Create AOIs, tiles, degrees
 # slim down Africa data
-africa <- st_read(file.path(data_path, "external/africa_noisl_gcs.sqlite")) %>%
-  st_sf(crs = 4326) %>% select(name, iso3) %>%
-  st_write("inst/extdata/africa.geojson")
+africa <- st_read("inst/extdata/africa.geojson")
 
 ## Set up mapping AOIs
 prod_groupings1 <- list(c(1216, 1217, 1283, 1284), c(1218, 1285), c(1219, 1286),
@@ -52,26 +54,26 @@ aois <- aois %>% group_by(grp) %>%
 aois <- aois %>%
   mutate(area = st_area(.) %>% units::set_units("km2") %>% as.numeric) %>%
   select(grp, aois, area)
-mean(aois$area)
-range(aois$area)
+# mean(aois$area)
+# range(aois$area)
 
 st_write(aois, dsn = "inst/extdata/aois.geojson")
 
 ## Write out 1 degree grids
-degrees <- raster(file.path(data_path, "processed/africa_aois.tif"))
+degrees <- raster(here::here("external/data/africa_aois.tif"))
 degrees <- rasterToPolygons(degrees) %>% st_as_sf %>%
   rename(grid = africa_aois) %>% st_sf(crs = 4326)
 st_write(degrees, "inst/extdata/degrees.geojson")
 
 ## Intersect AOIs with tiles to get AOI ID with tiles
-tiles <- read_sf(here::here("inst/extdata/ghana_tiles.geojson"))
+# tiles <- read_sf(here::here("inst/extdata/ghana_tiles.geojson"))
 
 # which tiles intersect which AOIs.
 tile_pts <- st_intersection(tiles %>% st_centroid, aois %>% select(aois))
 tiles2 <- left_join(tiles, tile_pts %>% as_tibble %>% select(tile, aois)) %>%
   arrange(aois) %>% select(tile, aois, aoi) %>% rename(aoi1 = aois, aoi2 = aoi)
-file.copy("inst/extdata/ghana_tiles.geojson",
-          "external/reference/ghana_tiles.geojson")
+# file.copy("inst/extdata/ghana_tiles.geojson",
+#           "external/reference/ghana_tiles.geojson")
 st_write(tiles2, "inst/extdata/ghana_tiles.geojson", delete_dsn = TRUE)
 
 ## get copy of primary grid in csv format
